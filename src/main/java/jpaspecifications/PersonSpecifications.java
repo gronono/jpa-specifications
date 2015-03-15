e125419eb1ab82fa32a64f3887a3b938982ce21b
Arnaud Brunet <arnaud.brunet@gouv.nc>
2015-03-16 10:02:39 +1100

    null

--- src/main/java/jpaspecifications/PersonSpecifications.java
+++ src/main/java/jpaspecifications/PersonSpecifications.java
@@ -3,6 +3,7 @@ package jpaspecifications;
 import java.time.LocalDate;
 
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
 
 import static jpaspecifications.Person.PROPERTYNAME_BIRTHDATE;
 import static jpaspecifications.Person.PROPERTYNAME_FIRSTNAME;
@@ -13,7 +14,7 @@ public class PersonSpecifications {
 	public static Specification<Person> lastnameContains(String content) {
 		return contains(PROPERTYNAME_LASTNAME, content);
 	}
-	
+
 	public static Specification<Person> firstnameContains(String content) {
 		return contains(PROPERTYNAME_FIRSTNAME, content);
 	}
@@ -21,8 +22,8 @@ public class PersonSpecifications {
 	public static Specification<Person> isBornOn(LocalDate date) {
 		return (root, query, cb) -> cb.equal(root.get(PROPERTYNAME_BIRTHDATE), date);
 	}
-	
+
 	private static Specification<Person> contains(String propertyname, String content) {
-		return (root, query, cb) -> cb.like(cb.lower(root.get(propertyname)), "%" + content.toLowerCase() + "%");
+		return !StringUtils.hasText(content) ? null :  (root, query, cb) -> cb.like(cb.lower(root.get(propertyname)), "%" + content.toLowerCase() + "%");
 	}
 }
